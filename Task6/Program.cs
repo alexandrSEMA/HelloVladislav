4aa0cf0cdc07e54192a2f1a76b780dd7d57f1200
Alexandr <a.semashkev14@gmail.com>
2022-09-02 19:09:47 +0300

    task 6 mission completed

---
+++ Task6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+string nameA = Console.ReadLine();
+int a = Convert.ToInt32(nameA);
+if (a % 2 == 0)
+{
+    Console.WriteLine("Число является четным");
+}
+else
+{
+    Console.WriteLine("Число не является четным");
+}
