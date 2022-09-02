a7c68923946e3ef4cc59002a2397b535f78302d6
Alexandr <a.semashkev14@gmail.com>
2022-09-02 19:00:20 +0300

    add Program.cs

---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите первое число");
+string nameA = Console.ReadLine();
+int a = Convert.ToInt32(nameA);
+Console.WriteLine("Введите второе число");
+string nameB = Console.ReadLine();
+int b = Convert.ToInt32(nameB);
+if (a > b)
+{
+    Console.Write("max = ");
+    Console.WriteLine(a);
+}
+else
+{
+    Console.Write("max = ");
+    Console.WriteLine(b);
+}
